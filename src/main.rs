245d93862dd3a9f012c62e20c678d0c45454c2e1
Julian Rosse <julian@helixbass.net>
2021-06-15 11:24:04 -0400

    expose virtual input port and debug-log received messages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use midir::os::unix::VirtualOutput;
-use midir::MidiOutput;
+use midir::os::unix::{VirtualInput, VirtualOutput};
+use midir::{MidiInput, MidiOutput};
 
 fn main() {
     let midi_out = MidiOutput::new("Line runner").unwrap();
 
     let /*mut*/ _conn_out = midi_out.create_virtual("Line runner").unwrap();
+
+    let midi_in = MidiInput::new("Line runner").unwrap();
+
+    let _conn_in = midi_in
+        .create_virtual(
+            "Line runner",
+            |stamp, message, _| {
+                println!("{}: {:?} (len = {})", stamp, message, message.len());
+            },
+            (),
+        )
+        .unwrap();
 
     loop {}
 }
