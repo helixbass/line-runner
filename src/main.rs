eda604f7995658a6071bb0fcfb7bacbf691ebaa9
Julian Rosse <julian@helixbass.net>
2021-06-15 15:07:29 -0400

    using arc + mutex

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use midir::os::unix::{VirtualInput, VirtualOutput};
 use midir::{MidiInput, MidiOutput};
+use std::sync::{Arc, Mutex};
 use wmidi::MidiMessage;
 
 use line_runner::message::Message;
@@ -11,12 +12,15 @@ fn main() {
 
     let midi_in = MidiInput::new("Line runner").unwrap();
 
+    let midi_clock_tracker = Arc::new(Mutex::new(MidiClockTracker::new()));
+    let midi_clock_tracker_clone = midi_clock_tracker.clone();
+
     let _conn_in = midi_in
         .create_virtual(
             "Line runner",
-            |timestamp, bytes, _| {
+            move |timestamp, bytes, _| {
                 if let Some(message) = Message::from(timestamp, bytes).unwrap() {
-                    handle_message(message);
+                    handle_message(message, &midi_clock_tracker_clone);
                 }
             },
             (),
@@ -26,11 +30,26 @@ fn main() {
     loop {}
 }
 
-fn handle_message(message: Message) -> () {
+fn handle_message(message: Message, midi_clock_tracker: &Arc<Mutex<MidiClockTracker>>) -> () {
     match message.message {
         MidiMessage::TimingClock => {
             println!("got timing clock message");
+            midi_clock_tracker.lock().unwrap().tick();
         }
         _ => {}
+    }
+}
+
+struct MidiClockTracker {
+    ticks_received: u32,
+}
+
+impl MidiClockTracker {
+    pub fn new() -> Self {
+        Self { ticks_received: 0 }
+    }
+
+    pub fn tick(&mut self) -> () {
+        self.ticks_received += 1;
     }
 }
