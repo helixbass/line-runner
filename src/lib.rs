ac6a3dc9943b924cd9bf31e7f246dbf3ba37b88c
Peter Stuart <peter@peterstuart.org>
2021-07-02 13:01:22 -0400

    Add a "model" module

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 mod beat_number;
-mod line;
 mod line_launcher;
-mod line_parser;
 mod message;
 mod midi_clock_tracker;
+mod model;
 mod result;
 
 pub use beat_number::BeatNumber;
-pub use line::{Line, LineNote};
 pub use line_launcher::LineLauncher;
-pub use line_parser::parse_line;
 pub use message::Message;
 pub use midi_clock_tracker::MidiClockTracker;
+pub use model::{
+    line::{Line, LineNote},
+    line_parser::parse_line,
+};
 pub use result::Result;
--- src/model/line.rs
+++ src/model/line.rs
@@ -18,15 +18,15 @@ impl Line {
     pub fn new(notes: Vec<LineNote>) -> Self {
         Self { notes }
     }
-}
 
-pub fn all_lines() -> Vec<Line> {
-    vec![
-        "C4 F3 G3 Bb3 C4 Db4 Eb4 F4 E4 . . .",
-        "- Db4 Bb3 Db4 C4 . Bb3 G3 F3 Bb3 F3 Gb3 G3 Gb3 F3 G3 E3 . . .",
-        "C4 F3 G3 Bb3 C4 Db4 Bb3 Db4 C4 . .",
-    ]
-    .iter()
-    .map(|line_str| parse_line(line_str).unwrap())
-    .collect()
+    pub fn all() -> Vec<Line> {
+        vec![
+            "C4 F3 G3 Bb3 C4 Db4 Eb4 F4 E4 . . .",
+            "- Db4 Bb3 Db4 C4 . Bb3 G3 F3 Bb3 F3 Gb3 G3 Gb3 F3 G3 E3 . . .",
+            "C4 F3 G3 Bb3 C4 Db4 Bb3 Db4 C4 . .",
+        ]
+        .iter()
+        .map(|line_str| parse_line(line_str).unwrap())
+        .collect()
+    }
 }
